52f3fc282c5c4271ef968cf89203e5109fd301cf
Manish jain <manishjain@nuclaysolutions.com>
2022-02-14 12:18:56 +0530

    deleted diff and mult

--- diff.php
+++ diff.php
@@ -1,6 +1,6 @@
 <?php
     function diff($num1,$num2)
     {
-        echo ("the diff is".$num1-$num2);
+        echo ("The difference is".$num1-$num2);
     }
 ?>
---
+++ mult.php
@@ -0,0 +1,6 @@
+<?php
+    function mult($num1,$num2)
+    {
+        echo ("The multiply is".$num1-$num2);
+    }
+?>
